dd33d3550ba378bd83bef5a6126c68e3cf922d12
jawah <jawaheralsaadi1@gmail.com>
2026-04-13 20:54:02 +0400

    Add Solution Task 1 - Create and Add Elements to Java Set

---
+++ src/productSetDemo.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+//Task 1 Method
+public class productSetDemo {
+      public static void main (String[] args){
+       Set<String> pCodes = new HashSet<>();
+       pCodes.add ("P1");
+       pCodes.add ("P2");
+       pCodes.add ("P3");
+       pCodes.add ("P4");
+       pCodes.add ("P5");
+       System.out.println(" Set  : " + pCodes);
+       pCodes.add ("P6");
+       System.out.println(" New Set  : " + pCodes);
+
+
+      }
+
+//Task 2 Method
+// Task 3 Method
+
+// Task 4 Method :
+
+// Task 5 Method :
+
+
+// Task 6 Method
+
+}
